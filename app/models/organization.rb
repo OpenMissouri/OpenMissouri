dbd4ae36674531237ce5dda5f8bf0598f57a71a2
Jamie Stephens <jamie@blueroot.com>
2011-03-04 10:48:58 -0600

    more lmits

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ActiveRecord::Base
   belongs_to :user
   has_many :data_sets, :order => "created_at desc"
-  has_many :published_data_sets, :class_name => "DataSet", :conditions => {:status => "published"}, :order => "created_at desc"
+  has_many :published_data_sets, :class_name => "DataSet", :conditions => {:status => "published"}, :order => "created_at desc", :limit => 20
   
   belongs_to :government_level
   
