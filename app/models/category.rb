2bb79c702dd094fb9cbbf04d69da615360d7e1cf
Jamie Stephens <jamie@blueroot.com>
2011-03-04 10:39:48 -0600

    pub cats

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :data_sets
 
   def self.published_categories  
-    find(:all, :conditions => "id in (#{DataSet.find(:all).collect{|data_set| data_set.categories.collect{|category| category.id}}.flatten.join(",")})", :order => "created_at desc")
+    find(:all, :conditions => "id in (#{DataSet.find(:all, :conditions => {:status => "published"}).collect{|data_set| data_set.categories.collect{|category| category.id}}.flatten.join(",")})", :order => "name")
   end 
 end
