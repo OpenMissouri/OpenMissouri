94d7b9c88afbc37b9b94c5560a28c16721ad948b
Caley Woods <caley.w@gmail.com>
2011-06-20 09:00:28 -0700

    Changed data_set model to use activesupport 'parameterize' inflector.

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -22,6 +22,8 @@ class DataSet < ActiveRecord::Base
   #end
   
   def to_param
-		"#{id}-#{name.gsub(/[^a-z0-9]+/i, '-').downcase}"
-	end
+    # parameterize prettifies the url and downcases
+    "#{id}-#{name.paramaterize}"
+  end
+
 end
