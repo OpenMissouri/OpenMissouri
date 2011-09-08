6e2eba7c8737b8da4073bee377745c9e6340c125
Jamie Stephens <jamie@blueroot.com>
2011-09-08 10:26:38 -0500

    adding google verification for google apps

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,1 +1,12 @@
 ActionMailer::Base.default_url_options[:host] = configatron.host
+
+
+ActionMailer::Base.smtp_settings = {  
+  :address              => "smtp.gmail.com",  
+  :port                 => 587,  
+  :domain               => "openmissouri.org",  
+  :user_name            => "openmissouri",  
+  :password             => "something",  
+  :authentication       => "plain",  
+  :enable_starttls_auto => true  
+}
